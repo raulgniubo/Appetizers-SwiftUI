9945e636624675265cb9a58de44e57332ef4366e
Raul Gutierrez Niubo <raulgniubo@gmail.com>
2021-09-18 13:15:01 -0400

    xcode project added

---
+++ Appetizers/Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Raul Gutierrez Niubo on 9/18/21.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
